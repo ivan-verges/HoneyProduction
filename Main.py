340a156e77b73a4aa254c60e89f083287e83b093
Ivan Verges <verges1@gmail.com>
2021-12-06 02:36:22 -0400

    Fix in Comments.

--- Main.py
+++ Main.py
@@ -25,21 +25,33 @@ if not os.path.exists(file_path):
     #Downloads the DataSet to the Defined Path
     wget.download(url, file_path)
 
+"""
+DataSet Columns:
+State => Location (United States)
+NumCol => Number of Hives
+YieldPerCol => Production Per Hive (Lb / year)
+Total Prod => Total Production (NumCol * YieldPerCol)
+Stocks => Available From Production
+PricePerLB => Price Per LB
+ProdValue => Value of Total Production (TotalProd * PricePerLB)
+Year => Year of Production
+"""
+
 #Loads the Data from the CSV File into a Dataframe using Pandas
 df = pd.read_csv(file_path)
 
 #Groups the DataFrame Data by Year and the Mean of the other columns
 prod_per_year = df.groupby(["year"]).mean().reset_index()
 
-#Creates the Features (Year) from data and formats it to be in the form of array ([[0, 1, 2]])
+#Creates the Features (Year) from data and formats it to be in the form of array ([[1998], [1999], [2000]]])
 X = prod_per_year["year"]
 X = X.values.reshape(-1, 1)
 
-#Creates the Labels (Total Production) for the Features and formats it to be in the form of array ([[0, 1, 2]])
+#Creates the Labels (Total Production) for the Features and formats it to be in the form of array ([[1998], [1999], [2000]])
 y = prod_per_year["totalprod"]
 y = y.values.reshape(-1, 1)
 
-#Generates a years time-lapse to predict and formats it to be in the form of array ([[0, 1, 2]])
+#Generates a years time-lapse to predict and formats it to be in the form of array ([[2013], [2014], [2015]])
 X_future = np.array(range(2013, 2027))
 X_future = X_future.reshape(-1, 1)
 
@@ -69,10 +81,10 @@ plt.xlabel("Year")
 plt.ylabel("Honey Production")
 
 #Set the graph to plot our data and our future predictions
-plt.scatter(df["year"], df["totalprod"], color='skyblue', linewidth=4, label="Productions by Year")
-plt.scatter(X, y, color='red', linewidth=6, label="Avg. Productions by Year")
-plt.plot(X, y_predict, color='blue', linewidth=4, label="Actual Predictions")
-plt.plot(X_future, future_predict, color='orange', linewidth=4, linestyle='dashed', label="Future Predictions")
+plt.scatter(df["year"], df["totalprod"], color='skyblue', linewidth=4, label="Production by Year")
+plt.scatter(X, y, color='red', linewidth=6, label="Avg. Production by Year")
+plt.plot(X, y_predict, color='blue', linewidth=4, label="Actual Prediction")
+plt.plot(X_future, future_predict, color='orange', linewidth=4, linestyle='dashed', label="Future Prediction")
 
 #Show Legends
 plt.legend()
